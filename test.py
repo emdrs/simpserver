d85880f82cd4c89389e75ff339ef3f42cdcfc9e7
manel <manelzaum@icloud.com>
2026-01-22 11:08:25 -0300

    add: some todos.

--- test.py
+++ test.py
@@ -2,6 +2,13 @@ from sys import argv
 from handler import serve_api
 
 
+"""
+TODO:
+    1. Get request body data.
+    2. Create ensure body keys decorator.
+    3. Get request params in url (GET Method).
+"""
+
 if len(argv) < 3 or not argv[2].isnumeric():
     print("WRONG USAGE!")
     print("python3 <file> ip port")
