5bcb8fe5f76f0969c937201d0a6bcded0fe7476d
manel <manelzaum@icloud.com>
2026-01-23 16:51:35 -0300

    add: goal.

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ TODO:
     2. Create ensure body keys decorator.
     3. Get request params in url (GET Method).
     4. Handle exceptions to return hints of errors and properly status code..
+    5. Create middleware decorator.
 """
 
 if len(argv) < 3 or not argv[2].isnumeric():
